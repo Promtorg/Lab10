f18ba94da46ba9e8d2f728128809d9574c5675dd
Promtorg <72571338+Promtorg@users.noreply.github.com>
2021-04-12 09:24:31 +0300

    Update QuEquationTest.php

--- tests/QuEquationTest.php
+++ tests/QuEquationTest.php
@@ -2,7 +2,7 @@
 
 use PHPUnit\Framework\TestCase;
 use Suslov\Exception;
-use Suslov\Square;
+use Suslov\QuEquation;
 
 class QuEquationTest extends TestCase
 {
@@ -11,13 +11,13 @@ class QuEquationTest extends TestCase
         $QuEquationEx = new QuEquation();
         $this->expectExceptionMessage("Ошибка: уравнение не имеет корней.");
         $this->expectException(Exception::class);
-        $squareEx->solve(4, 0, 8);
-        $squareEx->solve(4, 2, 1);
+        $QuEquationEx->solve(4, 0, 8);
+        $QuEquationEx->solve(4, 2, 1);
     }
 
     public function testQuEquation()
     {
-        $QuEquation = new QuEquation();
+        $QuEquationEx = new QuEquation();
         $this->assertEquals([5, -5], $QuEquation->solve(-3, 0, 75));
         $this->assertEquals([-3], $QuEquation->solve(0, 3, 9));
         $this->assertEquals([-3], $QuEquation->solve(1, 6, 9));
